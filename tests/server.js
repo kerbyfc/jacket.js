58dc2bdb3d7c2191c74de12f411140e73f69002e
kerbyfc <kerbyfc@gmail.com>
2013-05-07 20:03:40 +0400

    return server.js, change it, change package.js

---
+++ tests/server.js
@@ -0,0 +1,18 @@
+var connect = require('connect');
+var connectRoute = require("connect-route");
+
+var server = connect (
+  
+  connect.bodyParser(),
+  connect['static'](__dirname),
+
+  connectRoute( function(app) {
+    app.post('/error', function(req, res){
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('');
+    });
+  })
+
+);
+
+server.listen(8080);
